ace52e496a87ab74addfa62b0db73be96d2f1648
lingchao <75912001@qq.com>
2026-06-14 00:43:44 +0800

    Rename FramesInfo to PlayInfo and update related references for clarity

--- scripts/config/texturepacker.frame.gd
+++ scripts/config/texturepacker.frame.gd
@@ -2,6 +2,6 @@ class_name TexturePackerFrame
 extends RefCounted
 
 # TexturePacker 单帧播放数据.
-# ConfigPet / ConfigCharacter 在配置加载阶段扫描 `.tpsheet`, 并把 region, margin 和 offsets 合成为播放器可直接使用的数据.
+# ConfigAssets 在 assets 加载阶段扫描 `.tpsheet`, 并把 region, margin 和 offsets 合成为播放器可直接使用的数据.
 var region: Rect2
 var draw_position: Vector2
