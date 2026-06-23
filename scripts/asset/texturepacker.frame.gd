15ff25a72d8b6a649b37360894f42ba7eee9f1b9
lingchao <75912001@qq.com>
2026-06-23 10:54:54 +0800

    Add role configuration file with level ranges

--- scripts/asset/texturepacker.frame.gd
+++
@@ -1,8 +0,0 @@
-class_name TexturePackerFrame
-extends RefCounted
-
-# TexturePacker 单帧播放数据.
-# AssetManager 在启动扫描 `.tpsheet` 时把 region, margin 和 offsets 合成为播放器可直接使用的数据.
-var region: Rect2
-var draw_position: Vector2
-
