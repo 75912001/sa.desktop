20e00ae97c3878932e45d803ed84012188184e1a
lingchao <75912001@qq.com>
2026-06-23 10:54:27 +0800

    Add role configuration file with level ranges

--- config/texturepacker.frame.gd
+++
@@ -1,9 +0,0 @@
-class_name TexturePackerFrame
-extends RefCounted
-
-# TexturePacker 单帧播放数据.
-# AssetsConfig 在 assets 加载阶段扫描 `.tpsheet`, 并把 region, margin 和 offset 合成为播放器可直接使用的数据.
-var region: Rect2
-# anchor_position 表示锚点在当前裁剪帧内部的位置.
-# region 负责说明从 atlas 取哪块图, anchor_position 负责说明这块图自己的锚点在哪里.
-var anchor_position: Vector2
